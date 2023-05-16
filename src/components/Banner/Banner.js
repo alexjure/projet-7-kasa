0f69e48935deb9050bcfb221abaa1a56be3156b3
Alex Jure <alexjuredev@gmail.com>
2023-05-16 15:10:31 +0200

    Rectifications CSS, ajout compteur image

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,14 @@
 import './Banner.css'
-import imageBanniere from '../../assets/img-home.jpg'
+//import imageBanniere from '../../assets/img-home.jpg'
 
 function Banner() {
     return (
         <div className='banner'>
             <div className='cartBanner'>
-            <p className='textBanner'>Chez vous, partout et ailleurs</p>
+            <p className='textBanner' style={{backgroundImage: "url(../assets/img-home.jpg"}}> Chez vous, partout et ailleurs </p>
             </div>           
         </div>
     )
 }
 
 export default Banner
-
--- src/components/Carrousel/Carrousel.js
+++ src/components/Carrousel/Carrousel.js
@@ -27,9 +27,16 @@ const Carrousel = ({ slides }) => {
                 return (
                     <div className={index === current ? 'slide-active' : 'slide'} key={index}>
                         <div className='carte-image-carrousel' style={{ backgroundImage: `url(${l})` }}>
-                            <span onClick={prevSlide} style={{ display: display }} className='prev'><i class="fa-sharp fa-solid fa-chevron-left"></i></span>
-                            <span onClick={nextSlide} style={{ display: display }} className='next'><i class="fa-sharp fa-solid fa-chevron-right"></i></span>
-
+                            <div className='partPrev'>
+                                <span onClick={prevSlide} style={{ display: display }} className='prev'><i className="fa-sharp fa-solid fa-chevron-left"></i></span>
+                            </div>
+                            <div className='counterImage'>
+                                <p className="counter">{current + 1} / {slides.length}</p>
+                            </div>
+                            <div className='partNext'>
+                                <span onClick={nextSlide} style={{ display: display }} className='next'><i className="fa-sharp fa-solid fa-chevron-right"></i></span>
+                            </div>
+                            
                         </div>
                     </div>
                 )
@@ -41,6 +48,36 @@ const Carrousel = ({ slides }) => {
 export default Carrousel;
 
 /*
+
+idée pour la mise en place du compteur
+
+                    <div className='carte-image-carrousel' style={{ backgroundImage: `url(${l})` }}>
+                        <div className='toto'>        
+                            <span onClick={prevSlide} style={{ display: display }} className='prev'><i className="fa-sharp fa-solid fa-chevron-left"></i></span>
+                        </div>
+                        <div className='tata'>      
+                            < span onClick={nextSlide} style={{ display: display }} className='next'><i className="fa-sharp fa-solid fa-chevron-right"></i></span>
+                        </div>     
+                        <div className='counterImage'>
+                            <p className="counter">{current + 1} / {slides.length}</p>
+                        </div>
+                    </div>
+
+
+comment s'était à l'origine
+
+                        <div className='carte-image-carrousel' style={{ backgroundImage: `url(${l})` }}>
+                            <span onClick={prevSlide} style={{ display: display }} className='prev'><i className="fa-sharp fa-solid fa-chevron-left"></i></span>
+                            <span onClick={nextSlide} style={{ display: display }} className='next'><i className="fa-sharp fa-solid fa-chevron-right"></i></span>
+                            <div className='counterImage'>
+                                <p className="counter">{current + 1} / {slides.length}</p>
+                            </div>
+                        </div>
+
+
+
+
+
 <div className={index === current ? 'slide-active' : 'slide'} key={index}>
                         <div className='carte-image' style={{ backgroundImage: `url(${l})` }}>
                             <button onClick={nextSlide} style={{ display: display }}>Next</button>
